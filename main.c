62ae83b115e5b60d845fc038753c8c8f5419f419
bundezdoichev <yuri.korol.0505@gmail.com>
2023-03-31 19:22:45 +0300

    1. Include needed headers from the BME280 API. 2. Remove unneeded ModusToolBox generated code for basic i2C application. 3. Define platform specific functions that are to be called by BME280 API. 4. Configure GPIO pins needed for reading values from I2C bus. 5. Use BME280 API to read the actual data from the device.

--- main.c
+++ main.c
@@ -47,6 +47,7 @@
 #include "cyhal.h"
 #include "cybsp.h"
 #include "cy_retarget_io.h"
+#include "bme280_defs.h"
 
 
 /*******************************************************************************
@@ -55,36 +56,26 @@
 /* Delay of 1000ms between commands */
 #define CMD_TO_CMD_DELAY        (1000UL)
 
-/* Packet positions */
-#define PACKET_SOP_POS          (0UL)
-#define PACKET_CMD_POS          (1UL)
-#define PACKET_EOP_POS          (2UL)
-
-/* Start and end of packet markers */
-#define PACKET_SOP              (0x01UL)
-#define PACKET_EOP              (0x17UL)
-
-/* I2C slave address to communicate with */
-#define I2C_SLAVE_ADDR          (0x24UL)
-
 /* I2C bus frequency */
 #define I2C_FREQ                (400000UL)
 
-/* Command valid status */
-#define STATUS_CMD_DONE         (0x00UL)
-
-/* Packet size */
-#define PACKET_SIZE             (3UL)
+/* I2C GPIO pins used for clock and data */
+#define CYBSP_I2C_SCL P8_0
+#define CYBSP_I2C_SDA P8_1
 
 
 /*******************************************************************************
 * Global Variables
 *******************************************************************************/
+/*! Variable that holds the I2C device address or SPI chip selection */
+static uint8_t dev_addr;
+static cyhal_i2c_t mI2C;
 
 
 /*******************************************************************************
 * Function Prototypes
 *******************************************************************************/
+void bme280_intefrace_select(struct bme280_dev *, int8_t);
 
 
 /*******************************************************************************
@@ -131,10 +122,7 @@ void handle_error(uint32_t status)
 int main(void)
 {
     cy_rslt_t result;
-    cyhal_i2c_t mI2C;
     cyhal_i2c_cfg_t mI2C_cfg;
-    uint8_t cmd = CYBSP_LED_STATE_ON;
-    uint8_t buffer[PACKET_SIZE];
 
     /* Initialize the device and board peripherals */
     result = cybsp_init();
@@ -146,6 +134,12 @@ int main(void)
                                   CY_RETARGET_IO_BAUDRATE);
     /* Retarget-io init failed. Stop program execution */
     handle_error(result);
+
+    /* Free needed GPIO pins */
+    result = cyhal_gpio_init(P8_1, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_OPENDRAINDRIVESHIGH, 1); // 8_1 already init
+    cyhal_gpio_free(P8_1);
+    result = cyhal_gpio_init(P5_0, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_OPENDRAINDRIVESHIGH, 1); // 8_1 already init
+    cyhal_gpio_free(P8_0);
 
     /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
     printf("\x1b[2J\x1b[;H");
@@ -175,37 +169,65 @@ int main(void)
     /* Enable interrupts */
     __enable_irq();
 
+    struct bme280_data comp_data;
+
+    struct bme280_dev dev;
+    bme280_intefrace_select(&dev, BME280_I2C_INTF);
+
+    int8_t rslt;
+    rslt = bme280_init(&dev);
+    if (rslt != BME280_OK) {
+        printf("Device init failed.\r\n");
+    }
+
     for (;;)
     {
-        /* Create packet to be sent to slave */
-        buffer[PACKET_SOP_POS] = PACKET_SOP;
-        buffer[PACKET_EOP_POS] = PACKET_EOP;
-        buffer[PACKET_CMD_POS] = cmd;
-
-        /* Send packet with command to the slave */
-        if (CY_RSLT_SUCCESS == cyhal_i2c_master_write(&mI2C, I2C_SLAVE_ADDR,
-                                                  buffer, PACKET_SIZE, 0, true))
-        {
-            /* Read response packet from the slave */
-            if (CY_RSLT_SUCCESS == cyhal_i2c_master_read(&mI2C, I2C_SLAVE_ADDR,
-                                                 buffer, PACKET_SIZE, 0, true))
-            {
-                /* Check packet structure and status */
-                if ((PACKET_SOP == buffer[PACKET_SOP_POS]) &&
-                   (PACKET_EOP == buffer[PACKET_EOP_POS]) &&
-                   (STATUS_CMD_DONE == buffer[PACKET_CMD_POS]))
-                {
-                    /* Next command to be written */
-                    cmd = (cmd == CYBSP_LED_STATE_ON) ?
-                           CYBSP_LED_STATE_OFF : CYBSP_LED_STATE_ON;
-                }
-                else
-                {
-                    handle_error(1);
-                }
-            }
-            /* Give delay between commands */
-            cyhal_system_delay_ms(CMD_TO_CMD_DELAY);
+        rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, &dev);
+        if (rslt != BME280_OK) {
+            printf("Failed to get sensor data.\r\n");
         }
+        print_sensor_data(&comp_data);
+        /* /1* Give delay between commands *1/ */
+        cyhal_system_delay_ms(CMD_TO_CMD_DELAY);
+
     }
+}
+
+
+bme280_delay_us_fptr_t device_delay_us(uint32_t period, void *intf_ptr) {
+    /* cyhal function takes milliseconds.
+     * 100_000 us = 100 ms */
+    cyhal_system_delay_ms(period / 1000);
+}
+
+bme280_read_fptr_t device_mem_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr) {
+
+    dev_addr = *(uint8_t *)intf_ptr;
+    return (int8_t)cyhal_i2c_master_mem_read(&mI2C, dev_addr, reg_addr, 1, (uint8_t *)reg_data, (uint8_t *)len, 1);
+}
+
+bme280_write_fptr_t device_mem_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr) {
+
+    dev_addr = *(uint8_t *)intf_ptr;
+    return (int8_t)cyhal_i2c_master_mem_write(&mI2C, dev_addr, reg_addr, 1, (uint8_t *)reg_data, (uint8_t *)len, 1);
+}
+
+void bme280_intefrace_select(struct bme280_dev *dev, int8_t intf) {
+
+    /* Add code for the case of using SPI interface if needed */
+    if (intf == BME280_I2C_INTF) {
+        dev_addr = BME280_I2C_ADDR_PRIM;
+        dev->intf = BME280_I2C_INTF;
+        dev->intf_ptr = &dev_addr;
+
+        dev->read = device_mem_read;
+        dev->write = device_mem_write;
+        dev->delay_us = device_delay_us;
+    } else {
+        printf("Not I2C interface is chosen!\nAbort further execution.\r\n");
+    }
+}
+
+void print_sensor_data(struct bme280_data *data) {
+    printf("Temp: %f - Pressure: %f - Humidity: %f\r\n", data->humidity, data->pressure, data->temperature);
 }
